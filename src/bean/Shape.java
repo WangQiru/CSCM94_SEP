b2f5400adddf51b85f85f2f434a231989410a804
Wang Qiru <wqr624@gmail.com>
2016-02-25 10:42:54 +0000

    Updated class diagram with interfaces

--- src/bean/Shape.java
+++ src/bean/Shape.java
@@ -1,25 +1,13 @@
 package bean;
 
-public class Shape {
-	public int shapeId;
-	public String color;
-	public double diameter;
-	public int getShapeId() {
-		return shapeId;
-	}
-	public void setShapeId(int shapeId) {
-		this.shapeId = shapeId;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	public double getDiameter() {
-		return diameter;
-	}
-	public void setDiameter(double diameter) {
-		this.diameter = diameter;
-	}
+public interface Shape {
+	int shapeId;
+	String color;
+	double diameter;
+	public int getShapeId();
+	public void setShapeId(int shapeId);
+	public String getColor();
+	public void setColor(String color);
+	public double getDiameter();
+	public void setDiameter(double diameter);
 }
