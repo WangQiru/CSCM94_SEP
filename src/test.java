50b6c61d1de022ef0fe05fb34f9b1e96aff02a4e
Wang-Qiru <689404@Swansea.ac.uk>
2016-03-04 16:04:22 +0000

    Area; Graphics and Graphics2D;

--- src/test.java
+++ src/test.java
@@ -1,5 +1,12 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 
 public class test extends Frame{
 
@@ -22,10 +29,37 @@ public class test extends Frame{
 
 	public void paint(Graphics g) {
 		g.setColor(Color.red);
-		g.drawRect(50,50,200,200);
 		Graphics2D g2d = (Graphics2D)g;
+		g2d.rotate(Math.toRadians(45));
 		g2d.setColor(Color.blue);
-		g2d.drawRect(75,75,300,200);
+
+		int xPoly[] = {150,215,132,135,140,125,100};
+		int yPoly[] = {150,110,115,125,150,137,200};
+		Polygon p=new Polygon(xPoly,yPoly,7);
+		// New polygon
+		Ellipse2D.Double e=new Ellipse2D.Double(200,100,40,170);
+		// New ellipse
+
+
+		Area a=new Area(p);
+		Area ae=new Area(e);
+		// Convert shapes into Areas
+		ae.subtract(a);
+		/*
+		  Area:
+		   add
+		   subtract
+		   intersect
+		   transform
+
+		 */
+
+
+		g2d.draw(ae);
+//		g2d.fill(ae);
+		
+
+
 	}
 }
 
